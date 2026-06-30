f2cb7aa33f350727995e912c213977fc9eb52a53
RockerHX <rockerhx@gmail.com>
2026-06-30 09:35:28 +0800

    feat: 初始化 Tauri Vue 工程骨架

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,6 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
+    varfoldersf4z5176rn52p7cv3wrh6xhsr5m0000gnttmp9bk9nud7zwmotrix_fnos_lib::run()
+}
