25a0addac171196e2d9616ad721877e1d0757826
RockerHX <rockerhx@gmail.com>
2026-06-30 09:36:59 +0800

    chore: 安装前后端基础依赖

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    varfoldersf4z5176rn52p7cv3wrh6xhsr5m0000gnttmp9bk9nud7zwmotrix_fnos_lib::run()
+    motrix_fnos_lib::run()
 }
