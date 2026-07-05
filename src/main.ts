130a843cb91de75284e8f0e1e34688a2b2318f78
RockerHX <rockerhx@gmail.com>
2026-07-05 14:28:48 +0800

    feat: 添加移动端基线样式变量

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 import App from "./App.vue";
+import "./styles/mobile-baseline.css";
 
 createApp(App).use(createPinia()).mount("#app");
